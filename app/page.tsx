bc8a3874093dddc45deda924188079c4a1fc0c29
unknown <isaacjairi12@gmail.com>
2025-07-17 11:17:04 -0400

    remove about section

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <HeroSection />
       <Countdown />
       <WelcomeSection />
-      <AboutSection />
+      {/* <AboutSection /> */}
       <VenueSection />
       <GiftsSection />
       <Footer />
